c9ec9a328666ed0586e403796be6a9ad86d6a61f
Chirag Soni <csoni0693@gmail.com>
2025-06-08 10:56:22 +0530

    modified some changes

--- frontend/index.js
+++ frontend/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const path = require('path');
+const cors = require('cors')
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors())
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -23,9 +26,11 @@ app.post('/submit', async (req, res) => {
     const response = await axios.post('http://backend:5000/submit', { name, email });
     res.render('result', { data: response.data });
   } catch (error) {
+    console.error('Axios error:', error.response ? error.response.data : error.message);
     res.status(500).send('Error sending data to backend.');
   }
 });
+
 
 app.listen(PORT, () => {
   console.log(`Frontend running at http://localhost:${PORT}`);
