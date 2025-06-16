20a2d4411316cb08d7cf8826622b6f1c1b5d0cfc
Chirag Soni <csoni0693@gmail.com>
2025-06-16 17:55:35 +0530

    add

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,jsonify
+from flask import Flask,request,jsonify,render_template
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -11,6 +11,10 @@ def submit():
     email = data.get('email')
 
     return jsonify({"message":f"Received data => name: {name}, email: {email}"})
+
+@app.route("/")
+def view():
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000,debug=True)
